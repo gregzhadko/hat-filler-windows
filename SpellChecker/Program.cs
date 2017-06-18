8d3c538560f3f8d84ca97cf69209e37036b373ed
Grigory Zhadko <gregzhadko@gmail.com>
2017-06-18 22:08:20 +0200

    Added ReviewAllPacks method

--- SpellChecker/Program.cs
+++ SpellChecker/Program.cs
@@ -2,15 +2,17 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace SpellChecker
 {
     public class Program
     {
-        private static PackService _service = new PackService();
+        private static readonly PackService _service = new PackService();
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Green;
@@ -19,20 +21,37 @@ namespace SpellChecker
             var packs = LoadPacks();
             Console.WriteLine("Загрузка паков завершена");
 
-            //var lines = File.ReadAllLines(@"C:\Repo\Hat\HatDesktop\SpellChecker\CustomDictionary.txt");
-            //var uniqueLines = lines.Distinct().ToList();
-            //File.WriteAllLines(@"C:\Repo\Hat\HatDesktop\SpellChecker\CustomDictionary.txt", uniqueLines);
-
             var spellChecker = new SpellChecker(packs);
             spellChecker.Run();
 
-            //var localizationChecker = new LocalizationChecker();
-            //localizationChecker.Run(@"D:\Repo\hat-swift\Hat\Hat\Resources\en.lproj\Localizable.strings", @"D:\result.txt");
-
-            var editorFinder = new EditorFinder();
-            editorFinder.Run(packs);
+            //ReviewAllPacks(packs);
 
             Console.ReadKey();
+        }
+
+        private static void ReviewAllPacks(List<Pack> packs)
+        {
+            var authors = Reviewer.NewReviewers().Select(r => r.Author).ToList();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (var pack in packs.Where(p => p.Id == 13 || p.Id == 9 || p.Id == 10 || p.Id == 11))
+            {
+                Console.WriteLine($"Смотрю пак {pack.Name}");
+                foreach (var phrase in pack.Phrases)
+                {
+                    Console.WriteLine($"Cмотрю фразу {phrase.Phrase}");
+                    foreach (var author in authors)
+                    {
+                        _service.ReviewPhrase(pack.Id, phrase, author, State.Accept);
+                    }
+                }
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
         }
 
         private static List<Pack> LoadPacks()
